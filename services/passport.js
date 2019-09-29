0927620707ab73d0a963257e5fea86ae567efe2e
KaranAhir9 <karanahir271994@gmail.com>
2019-09-29 20:55:13 +0530

    dont add duplicate users

--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const passport = require('passport');
 const mongoose = require('mongoose')
+
+
 const User = mongoose.model('users');
 
 
@@ -11,7 +13,14 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log(profile.id);
-        new User({ googleId: profile.id}).save();
+        User.findOne({ googleId: profile.id }).then( existingUser =>{
+            if(existingUser){
+                //We have the profile ID in our DB already
+            }else{
+                //create a new record with the Profile Id as we don't have the user record
+                new User({ googleId: profile.id }).save();
+            }
+        });
+
     })
 );
