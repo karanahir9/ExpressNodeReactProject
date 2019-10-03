7bd684f0f19d437f64d0c063bf07c16201adcdfb
KaranAhir9 <karanahir271994@gmail.com>
2019-10-03 21:15:10 +0530

    G strategy tweak

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//Dev.js Don commit this file.
+
+module.exports = {
+    googleClientID: '1085787198216-u9i9ulop4trr92tqeivdu6skuc3b8b74.apps.googleusercontent.com',
+    googleClientSecret: 'u4cgtcZKAHQNiSssl9uE5UO_',
+    mongoURI: 'mongodb+srv://emailyAdmin:Admin@emaily-yd2v0.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'aiosdjknjxdfkhjhfskckcdnhjsecfancvketRERWCGTRHR'
+};
